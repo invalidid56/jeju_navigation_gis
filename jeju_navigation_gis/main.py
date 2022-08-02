b61f3d33e9883770de162fbbed1ed6dcc262aaee
invalidid56 <karl2003@naver.com>
2022-08-02 14:24:40 +0900

    ulhi

--- jeju_navigation_gis/main.py
+++ jeju_navigation_gis/main.py
@@ -4,9 +4,14 @@
 
 import os
 import sys
-
+import geopandas as gpd
 import pandas as pd
 import re
+
+
+KEYS = ['std_ymd',	'arrival_hour',	'local_user',
+        'sex',	'age',	'org',	'dest',	'catecode_a',
+        'catecode_b',	'catecode_c',	'catecode_d']
 
 
 def main(data_dir, result_dir):
@@ -24,13 +29,22 @@ def main(data_dir, result_dir):
         else:
             sep = ','
 
-        df = pd.read_csv(file, sep=sep).head()
+        df = pd.read_csv(file, sep=sep)[:10]
+        cols = df.columns
 
         if sep == '|':
             col = [x.lower() for x in df.columns]
             df.columns = col
+        try:
+            temp.append(df[KEYS])
+        except KeyError:
+            print(file)
+            continue
 
-        temp.append(df)
+        if not os.path.exists('temp.csv'):
+            temp[-1].to_csv('temp.csv', mode='w', encoding='euc-kr')
+        else:
+            temp[-1].to_csv('temp.csv', mode='a', encoding='euc-kr')
 
     df = pd.concat(temp)
 
